fb9eaafb4bbb5fd7705fe67001c6922983152a1b
Artur <ArthurZ43@yandex.ru>
2023-02-20 17:07:08 +0300

    complete m2m relations

--- m2m-relations/articles/admin.py
+++ m2m-relations/articles/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 
-from .models import Article
+from .models import Article, Scope, Tag
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    extra = 1
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass
--- m2m-relations/articles/models.py
+++ m2m-relations/articles/models.py
@@ -1,12 +1,25 @@
 from django.db import models
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Раздел')
+
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+
+    def __str__(self):
+        return self.name
+
+
 class Article(models.Model):
 
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение')
+    tags = models.ManyToManyField(Tag, related_name='articles',
+                                  through='Scope')
 
     class Meta:
         verbose_name = 'Статья'
@@ -14,3 +27,12 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Scope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes', verbose_name='Раздел')
+    is_main = models.BooleanField(verbose_name='Основной')
+
+    class Meta:
+        ordering = ('-is_main',)
