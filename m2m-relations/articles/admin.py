8133005bccd17bec139473b3df302de9d69f4c3d
Artur <ArthurZ43@yandex.ru>
2023-02-22 11:55:08 +0300

    added count of main tags validation

--- m2m-relations/articles/admin.py
+++ m2m-relations/articles/admin.py
@@ -1,15 +1,34 @@
 from django.contrib import admin
+from django.forms.models import BaseInlineFormSet
+from django.core.exceptions import ValidationError
+
 
 from .models import Article, Scope, Tag
 
 
+class ScopeInlineFormSet(BaseInlineFormSet):
+    def clean(self):
+        main_check = tuple(
+            form.cleaned_data.get('is_main') for form in self.forms
+        )
+        print(main_check)
+        if main_check.count(True) < 1:
+            raise ValidationError('Выберите основной раздел.')
+        elif main_check.count(True) > 1:
+            raise ValidationError('Основным разделом может быть только один.')
+        super(ScopeInlineFormSet, self).clean()
+
+
 class ScopeInline(admin.TabularInline):
+    print("SCOPE inline")
     model = Scope
     extra = 1
+    formset = ScopeInlineFormSet
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    print("ADMIN article")
     inlines = [ScopeInline]
 
 
